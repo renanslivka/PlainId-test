46e1bf21d64cd2fbb764ad09e476072176b72bf8
renanslivka <renanslivka@gmail.com>
2020-08-20 17:36:07 +0300

    Updates

---
+++ precache-manifest.546c6bf0201613b4e0f9b286e7fd7537.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f99fab98166aa87efe973713925fb9",
+    "url": "/PlainId-test/index.html"
+  },
+  {
+    "revision": "af2e55954b8f4e4ff73a",
+    "url": "/PlainId-test/static/css/4.be2c9ac9.chunk.css"
+  },
+  {
+    "revision": "545f3247560800416d74",
+    "url": "/PlainId-test/static/css/6.eb873cc0.chunk.css"
+  },
+  {
+    "revision": "97004bc11f045a6a431b",
+    "url": "/PlainId-test/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "763cb4e1517ce4c09b47",
+    "url": "/PlainId-test/static/js/2.342ecdb4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PlainId-test/static/js/2.342ecdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6989fc94becf53e484f1",
+    "url": "/PlainId-test/static/js/3.cfd9e142.chunk.js"
+  },
+  {
+    "revision": "af2e55954b8f4e4ff73a",
+    "url": "/PlainId-test/static/js/4.fcede15b.chunk.js"
+  },
+  {
+    "revision": "ec786b9ae521ddc4caf6",
+    "url": "/PlainId-test/static/js/5.db8e071b.chunk.js"
+  },
+  {
+    "revision": "545f3247560800416d74",
+    "url": "/PlainId-test/static/js/6.5b190080.chunk.js"
+  },
+  {
+    "revision": "bdf50319a11e0a1dea21",
+    "url": "/PlainId-test/static/js/7.3542136c.chunk.js"
+  },
+  {
+    "revision": "97004bc11f045a6a431b",
+    "url": "/PlainId-test/static/js/main.125decf0.chunk.js"
+  },
+  {
+    "revision": "ac17711e97a8a24bbcab",
+    "url": "/PlainId-test/static/js/runtime-main.5e0d5dec.js"
+  }
+]);
---
+++ static/js/7.3542136c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpplainid-junior-assignment"]=this["webpackJsonpplainid-junior-assignment"]||[]).push([[7],{125:function(e,n,a){"use strict";a.r(n);var t=a(1),i=a.n(t),r=a(128),l=a(126),o=a(93);n.default=function(){return i.a.createElement(r.a,{p:3},i.a.createElement(o.a,{variant:"body2",color:"textSecondary",align:"center"},"Copyright \xa9 ",i.a.createElement(l.a,{href:"https://www.linkedin.com/in/renan-slivka",color:"inherit"},"Renan Slivka"," "),(new Date).getFullYear()))}}}]);
+//# sourceMappingURL=7.3542136c.chunk.js.map
